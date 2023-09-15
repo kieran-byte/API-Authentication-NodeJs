6330dc78f25767d9252805bcbe5099830170113b
Kieran-Byte <bettesworthkieran1@gmail.com>
2023-09-15 20:59:45 +1200

    changed imports

--- authServer.js
+++ authServer.js
@@ -3,8 +3,9 @@ const morgan = require('morgan') // middleware logger
 const createError = require('http-errors')
 require('dotenv').config()
 require('./helpers/init_mongodb')
-const { verifyAccessToken } = require('./helpers/jwt_helper')
+// const { verifyAccessToken } = require('./helpers/jwt_helper')
 require('./helpers/init_redis')
+const AuthMethods = require('./middleware/authMiddleware')
 
 const AuthRoute = require('./Routes/Auth.route')
 
@@ -13,11 +14,13 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', verifyAccessToken, async (req, res, next) => {
+
+app.use('/auth', AuthRoute)
+
+app.get('/', AuthMethods.verifyAccessToken, async (req, res, next) => {
   res.send('Hello from express.')
 })
 
-app.use('/auth', AuthRoute)
 
 app.use(async (req, res, next) => {
   next(createError.NotFound())
