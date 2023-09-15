b632d06f853e4563d087bda50ce7ae5c0366a390
Kieran-Byte <bettesworthkieran1@gmail.com>
2023-09-15 20:57:42 +1200

    removed verifyAccessToken

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -10,41 +10,22 @@ class TokenController{
     const payload = {};
     const secret = process.env.ACCESS_TOKEN_SECRET;
     const options = {
-      expiresIn: '1s',
+      expiresIn: '45s',
       issuer: 'pickurpage.com',
       audience: userId,
     };
 
     try {
-      const token = await jwt.sign(payload, secret, options);
-      console.log(`token in sign access token is ${token}`)
+      //attemps to sign the jwt and returns if successful 
+      const token = jwt.sign(payload, secret, options);
       return token;
     } catch (err) {
+      //otherwise throws an error upwards
       console.error(err.message);
-      // throw createError.InternalServerError();
+      throw createError.InternalServerError();
     }
   }
 
-  static async verifyAccessToken(req, res, next){
-    if (!req.headers['authorization']){
-      return next(createError.Unauthorized())
-    } 
-    
-    
-    const authHeader = req.headers['authorization']
-    const bearerToken = authHeader.split(' ')
-    const token = bearerToken[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-      
-      if (err) {
-        const message =
-          err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message
-        return next(createError.Unauthorized(message))
-      }
-      req.payload = payload
-      next()
-    })
-  }
 
 
   static async signRefreshToken(userId){
@@ -57,7 +38,7 @@ class TokenController{
     };
 
     try {
-      const token = await jwt.sign(payload, secret, options);
+      const token = jwt.sign(payload, secret, options);
 
       const expiresInSeconds = 365 * 24 * 60 * 60;
 
@@ -71,8 +52,9 @@ class TokenController{
 
       return token;
     } catch (err) {
+      //on fail throw upwards
       console.error(err.message);
-      // throw createError.InternalServerError();
+      throw createError.InternalServerError();
     }
   }
 
