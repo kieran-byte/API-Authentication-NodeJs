376f10fad6be608197d287f1df4d2fb501f625a2
Kieran-Byte <bettesworthkieran1@gmail.com>
2023-09-15 20:58:18 +1200

    added middleware to delete route

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const AuthController = require('../Controllers/Auth.Controller')
+const AuthMethods = require('../middleware/authMiddleware')
 
 router.post('/register', AuthController.register)
 
@@ -8,6 +9,6 @@ router.post('/login', AuthController.login)
 
 router.post('/refresh-token', AuthController.refreshToken)
 
-router.delete('/logout', AuthController.logout)
+router.delete('/logout', AuthMethods.verifyAccessToken ,AuthController.logout)
 
 module.exports = router
