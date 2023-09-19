b2228a5bd6af73f8920108b68b6d055461121cee
Kieran-Byte <bettesworthkieran1@gmail.com>
2023-09-19 12:11:34 +1200

    clean up and comments

--- Controllers/Auth.Controller.js
+++ Controllers/Auth.Controller.js
@@ -19,6 +19,7 @@ class AuthController{
   static async register(req, res, next){
 
     try{
+      //ensures input is valid, checks if email is already registered
       const result = await authSchema.validateAsync(req.body)
       const doesExist = await User.findOne({email: result.email})
 
@@ -26,11 +27,13 @@ class AuthController{
         throw createError.Conflict(`${result.email} is already in use`)
       }
 
+      //cretes and saves user
       const user = new User(result)
       const savedUser = await user.save()
       
-      const accessToken = await TokenController.signAccessToken(savedUser.id)
-      const refreshToken = await TokenController.signRefreshToken(savedUser.id)
+      //signs access and refresh tokens
+      const accessToken = TokenController.signAccessToken(savedUser.id)
+      const refreshToken = TokenController.signRefreshToken(savedUser.id)
       
       res.send({ accessToken, refreshToken })
 
@@ -57,7 +60,9 @@ class AuthController{
       if (!user){
         throw createError.NotFound('User not registered')
       } 
-    
+      
+      console.log(req.body.email)
+
       //check if there is a match in the passwords
       const isMatch = await user.isValidPassword(result.password) 
       if (!isMatch){
@@ -66,9 +71,9 @@ class AuthController{
       
       //get access and refresh tokens and return them
       console.log('generating access tokens')
-      const accessToken = await TokenController.signAccessToken(user.id) //this is returning undefined
+      const accessToken = TokenController.signAccessToken(user.id) //this is returning undefined
       console.log('passed on accessToken')
-      const refreshToken = await TokenController.signRefreshToken(user.id)
+      const refreshToken = TokenController.signRefreshToken(user.id)
       console.log('passed on refresh token')
       console.log(accessToken)
       res.send({ accessToken, refreshToken })
@@ -98,13 +103,13 @@ class AuthController{
       } 
 
       //get userId and sign tokens, return
-      const userId = await verifyRefreshToken(refreshToken)
+      const userId = await TokenController.verifyRefreshToken(refreshToken)
 
       console.log('not fail')
 
-      const accessToken = await TokenController.signAccessToken(userId)
+      const accessToken = TokenController.signAccessToken(userId)
       console.log('passed access token')
-      const refToken = await TokenController.signRefreshToken(userId)
+      const refToken = TokenController.signRefreshToken(userId)
       console.log('passed ref token')
       res.send({ accessToken: accessToken, refreshToken: refToken })
     } catch (error) {
@@ -117,19 +122,23 @@ class AuthController{
     
     console.log('in logout')
     
+    const refreshToken = req.body.refreshToken
+
     try {
-      const { refreshToken } = req.body
-      if (!refreshToken) throw createError.BadRequest()
-
+          
+      console.log('fail before user id')
       const userId = await TokenController.verifyRefreshToken(refreshToken)
-      console.log(`got userId ${userId}`)
+      
 
+      //const userId = payload.aud;
+      console.log('userID', userId)
+      
+      //this contains invalid arguments
       client.DEL(userId, (err, val) => {
         if (err) {
           console.log(err.message)
           throw createError.InternalServerError()
         }
-        console.log(val)
         res.sendStatus(204)
       })
     } catch (error) {
